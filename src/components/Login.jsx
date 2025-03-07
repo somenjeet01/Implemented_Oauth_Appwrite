c74b3b37af3122109487a3b0ebee39f6f7bb7f38
somenjeet01 <somenjeet2004@gmail.com>
2025-03-08 00:37:23 +0530

    Oauth_GitHub

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { account } from "../appwrite/appwriteConfig";
 import { useNavigate } from "react-router-dom";
-import {OAuthProvider } from "appwrite";
+import { OAuthProvider } from "appwrite";
 
 function Login() {
   const navigate = useNavigate();
@@ -19,27 +19,43 @@ function Login() {
       console.log(error);
     }
   };
-   
+
   const loginWithGoogle = async () => {
     try {
       // const redirectUrl = window.location.origin + "/auth/callback";
-      account.createOAuth2Session(
+
+      const val = await account.createOAuth2Session(
         OAuthProvider.Google,
         "http://localhost:5173/profile",
         "http://localhost:5173/"
       );
+      console.log("val ", val);
     } catch (error) {
       console.log("OAuth Error:", error);
     }
   };
 
+  const loginWithGitHub = async () => {
+    try {
+      // const redirectUrl = window.location.origin + "/auth/callback";
 
+      const val = await account.createOAuth2Session(
+        OAuthProvider.Github,
+        "http://localhost:5173/profile",
+        "http://localhost:5173/"
+      );
+      console.log("val ", val);
+    } catch (error) {
+      console.log("OAuth Error:", error);
+    }
+  };
 
   return (
     <>
       <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="text-center font-bold text-2xl">Log in</div>
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          []
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" action="#" method="POST">
               <div>
@@ -169,7 +185,7 @@ function Login() {
                 </div>
 
                 <div>
-                  <a
+                  <button
                     href="/"
                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                   >
@@ -177,34 +193,41 @@ function Login() {
                     <svg
                       className="w-5 h-5"
                       aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
+                      viewBox="0 0 24 24"
+                      fill="white"
                     >
-                      <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M12 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.234c-3.338.726-4.043-1.416-4.043-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.09-.744.083-.729.083-.729 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.805 1.304 3.49.996.108-.776.418-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.237-3.22-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.3 1.23a11.521 11.521 0 013.003-.403c1.02.005 2.046.137 3.003.403 2.292-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.873.118 3.176.77.84 1.236 1.91 1.236 3.22 0 4.61-2.805 5.626-5.478 5.92.43.37.812 1.103.812 2.22v3.293c0 .32.19.694.8.576C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12-12z"
+                      />
                     </svg>
-                  </a>
+                  </button>
                 </div>
 
                 <div>
-                  <a
-                    href="/"
+                  <button
+                    type="submit"
+                    onClick={loginWithGitHub}
                     className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
                   >
                     <span className="sr-only">Sign in with GitHub</span>
                     <svg
                       className="w-5 h-5"
                       aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
+                      viewBox="0 0 24 24"
+                      fill="black"
                     >
                       <path
                         fillRule="evenodd"
-                        d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
                         clipRule="evenodd"
+                        d="M12 0C5.373 0 0 5.373 0 12c0 5.302 3.438 9.8 8.207 11.387.6.11.793-.26.793-.577v-2.234c-3.338.726-4.043-1.416-4.043-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.09-.744.083-.729.083-.729 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.805 1.304 3.49.996.108-.776.418-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.467-2.38 1.237-3.22-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.3 1.23a11.521 11.521 0 013.003-.403c1.02.005 2.046.137 3.003.403 2.292-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.873.118 3.176.77.84 1.236 1.91 1.236 3.22 0 4.61-2.805 5.626-5.478 5.92.43.37.812 1.103.812 2.22v3.293c0 .32.19.694.8.576C20.565 21.796 24 17.3 24 12c0-6.627-5.373-12-12-12z"
                       />
                     </svg>
-                  </a>
+                  </button>
                 </div>
+
+
               </div>
             </div>
           </div>
